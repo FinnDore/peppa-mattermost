9061625574cced8a7ed1cb159649f335ee3daff2
Finn <34718806+FinnDore@users.noreply.github.com>
2023-01-07 20:52:58 +0000

    added pfp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { type AppType } from "next/app";
 
 import { trpc } from "../utils/trpc";
 
-import clsx from "clsx";
 import "../styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
@@ -15,8 +14,12 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <SessionProvider session={session}>
             <div className="absolute -z-10 h-full w-full bg-blue"></div>
             <div className="absolute -bottom-2/3 -left-1/2 -z-10 h-full w-[200%] -rotate-3 bg-green"></div>
-            <div className="mx-12 flex h-full flex-col ">
-                <div className="h-28 "></div>
+            <div className="mx-24 flex h-full flex-col ">
+                <div className="flex h-28 justify-end ">
+                    <div className="my-auto h-12">
+                        <Pfp src={"/what.png"} size={32} />
+                    </div>
+                </div>
                 <div className="relative z-10 mb-16 h-full">
                     <div className="h-full w-full rounded-[40px] border-[6px] border-black bg-pink bg-[url('/dots.svg')]"></div>
                     <div className="absolute top-2 left-2 -z-10 h-full w-full rounded-[40px]  bg-black"></div>
@@ -29,16 +32,19 @@ const MyApp: AppType<{ session: Session | null }> = ({
 
 export default trpc.withTRPC(MyApp);
 
-const Dots = ({ numberOfDots }: { numberOfDots: number }) => (
-    <div className="flex w-full justify-between">
-        {Array.from({ length: numberOfDots }, (_, index) => (
-            <div
-                key={index}
-                style={{}}
-                className={clsx(" h-1 w-1 rounded-full bg-black", {
-                    "opacity-50": index % 2 === 0,
-                })}
-            ></div>
-        ))}
-    </div>
-);
+const Pfp = ({ src, size = 32 }: { src: string; size: number }) => {
+    return (
+        <div className="relative  h-full w-full">
+            <div className="aspect-square h-full overflow-hidden  rounded-full border-[3px]  border-black">
+                <picture>
+                    <img
+                        className="h-full w-full object-cover"
+                        src={src}
+                        alt="Profile picture"
+                    />
+                </picture>
+                <div className="absolute top-[2px] left-[2px] -z-10 h-full w-full rounded-full bg-black"></div>
+            </div>
+        </div>
+    );
+};
