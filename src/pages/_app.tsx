70be4ff3daa9232e3cb7430e7dd4431c5e94ab42
Finn <34718806+FinnDore@users.noreply.github.com>
2023-01-07 22:27:42 +0000

    added users

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,14 +15,14 @@ const MyApp: AppType<{ session: Session | null }> = ({
         <SessionProvider session={session}>
             <div className="absolute -z-10 h-full w-full bg-blue"></div>
             <div className="absolute -bottom-2/3 -left-1/2 -z-10 h-full w-[200%] -rotate-3 bg-green"></div>
-            <div className="mx-24 flex h-full flex-col ">
+            <div className="mx-24 flex h-full flex-col">
                 <div className="flex h-28 justify-end ">
                     <div className="my-auto mr-4 h-12">
-                        <Pfp src={"/what.png"} />
+                        <Pfp src={"/pfp/what.png"} />
                     </div>
                 </div>
                 <div className="relative z-[-9] mb-16 h-full">
-                    <div className="h-full w-full rounded-[40px] border-[6px] border-black bg-pink bg-[url('/dots.svg')] px-8 pt-6">
+                    <div className="h-full w-full overflow-auto rounded-[40px] border-[6px] border-black bg-pink bg-[url('/dots.svg')] px-8 pt-6">
                         <Component {...pageProps} />
                     </div>
                     <div className="absolute top-2 left-2 -z-10 h-full w-full rounded-[40px]  bg-black"></div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,108 @@
+import clsx from "clsx";
 import { type NextPage } from "next";
 import { FC, PropsWithChildren } from "react";
 import { Pfp } from "../components/pfp";
 
-import { trpc } from "../utils/trpc";
+const users = {
+    admins: [
+        {
+            name: "finn",
+            pfp: "/pfp/what.png",
+        },
+        {
+            name: "Milo",
+            pfp: "/pfp/milo.webp",
+        },
+    ],
+    team: [
+        {
+            name: "Dhanajay",
+            pfp: "/pfp/dhanajay.webp",
+        },
+        {
+            name: "Medium",
+            pfp: "/pfp/medium.webp",
+        },
+        {
+            name: "A dumb",
+            pfp: "/pfp/dumb.webp",
+        },
+        {
+            name: "Neb",
+            pfp: "/pfp/neb.webp",
+        },
+        {
+            name: "jackson",
+            pfp: "/pfp/jackson.webp",
+        },
+        {
+            name: "Alaina",
+            pfp: "/pfp/alaina.webp",
+        },
+        {
+            name: "Far",
+            pfp: "/pfp/far.webp",
+        },
+    ],
+    unsorted: [
+        {
+            name: "Aleks",
+            pfp: "/pfp/aleks.webp",
+        },
+        {
+            name: "Wyz",
+            pfp: "/pfp/wyz.webp",
+        },
+    ],
+} as const;
 
 const Home: NextPage = () => {
-    const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
-
     return (
         <>
             <main>
                 <h1 className="mb-4 text-4xl font-bold">Users</h1>
-                <h2 className="text-2xl font-bold underline">Admins</h2>
-                <div>
-                    <Chip className="mr-2 bg-blue">
-                        <Pfp
-                            src="/what.png"
-                            small={true}
-                            className="mr-2 w-6"
-                        />
-                        Finn
-                    </Chip>
+                <h2 className="mb-4 text-2xl font-bold underline">Admins</h2>
+                <div className="mb-4 flex">
+                    {users.admins.map((user) => (
+                        <Chip className="mr-4" key={user.name}>
+                            <Pfp
+                                src={user.pfp}
+                                small={true}
+                                className="mr-2 w-6"
+                            />
+                            {user.name}
+                        </Chip>
+                    ))}
+                </div>
+                <h2 className="mb-4 text-2xl font-bold  underline">
+                    Team Members
+                </h2>
+                <div className="mb-4 flex flex-wrap">
+                    {users.team.map((user) => (
+                        <Chip className="mr-4 mb-4" key={user.name}>
+                            <Pfp
+                                src={user.pfp}
+                                small={true}
+                                className="mr-2 w-6"
+                            />
+                            {user.name}
+                        </Chip>
+                    ))}
+                </div>
+                <h2 className="mb-4 text-2xl font-bold  underline">
+                    Unsorted:
+                </h2>
+                <div className="mb-4 flex flex-wrap">
+                    {users.unsorted.map((user) => (
+                        <Chip className="mr-4 mb-4" key={user.name}>
+                            <Pfp
+                                src={user.pfp}
+                                small={true}
+                                className="mr-2 w-6"
+                            />
+                            {user.name}
+                        </Chip>
+                    ))}
                 </div>
             </main>
         </>
@@ -29,14 +111,17 @@ const Home: NextPage = () => {
 
 export default Home;
 
-const Chip: FC<PropsWithChildren<{ className?: string }>> = ({ children }) => {
+const Chip: FC<PropsWithChildren<{ className?: string }>> = ({
+    children,
+    className,
+}) => {
     return (
-        <div className="relative z-[9] w-min ">
+        <div className={clsx("relative z-[9] w-min min-w-max ", className)}>
             <div className="flex h-full rounded-[15px] border-[3px] border-black bg-blue p-2 px-2 pr-4">
                 {children}
             </div>
 
-            <div className="-z-1 absolute top-[2px] left-[2px] h-full w-full rounded-[15px] bg-black"></div>
+            <div className="absolute top-[2px] left-[2px] z-[-1] h-full w-full rounded-[15px] bg-black"></div>
         </div>
     );
 };
