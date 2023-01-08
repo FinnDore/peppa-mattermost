e92b75142b66f79d7c62b2a757ab81e012cc4b75
Finn <34718806+FinnDore@users.noreply.github.com>
2023-01-08 18:14:24 +0000

    dnd kinda works

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,12 +16,16 @@ const MyApp: AppType<{ session: Session | null }> = ({
             <div className="absolute -z-10 h-full w-full bg-blue"></div>
             <div className="absolute -bottom-2/3 -left-1/2 -z-10 h-full w-[200%] -rotate-3 bg-green"></div>
             <div className="mx-24 flex h-full flex-col">
+                <picture className="absolute -z-10 ">
+                    <img src="/peppa.png" alt="Peppa Pig" className="w-44" />
+                </picture>
                 <div className="flex h-28 justify-end ">
+                    <h1 className="my-auto mr-4 text-3xl font-bold">Finn</h1>
                     <div className="my-auto mr-4 h-12">
                         <Pfp src={"/pfp/what.png"} />
                     </div>
                 </div>
-                <div className="relative z-[-9] mb-16 h-full">
+                <div className="relative mb-16 h-full">
                     <div className="h-full w-full overflow-auto rounded-[40px] border-[6px] border-black bg-pink bg-[url('/dots.svg')] px-8 pt-6">
                         <Component {...pageProps} />
                     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
+/* eslint-disable react/display-name */
+import { DndContext, useDroppable } from "@dnd-kit/core";
+import {
+    rectSortingStrategy,
+    SortableContext,
+    useSortable,
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 import clsx from "clsx";
 import { type NextPage } from "next";
-import { FC, PropsWithChildren } from "react";
+import { CSSProperties, forwardRef, HTMLProps } from "react";
 import { Pfp } from "../components/pfp";
-
 const users = {
     admins: [
         {
@@ -57,52 +64,63 @@ const users = {
 } as const;
 
 const Home: NextPage = () => {
+    const { setNodeRef: adminsSetNodeRef } = useDroppable({ id: "admins" });
     return (
         <>
             <main>
-                <h1 className="mb-4 text-4xl font-bold">Users</h1>
+                <h1 className="mb-2 text-4xl font-bold">Users</h1>
                 <h2 className="mb-4 text-2xl font-bold underline">Admins</h2>
-                <div className="mb-4 flex">
-                    {users.admins.map((user) => (
-                        <Chip className="mr-4" key={user.name}>
-                            <Pfp
-                                src={user.pfp}
-                                small={true}
-                                className="mr-2 w-6"
-                            />
-                            {user.name}
-                        </Chip>
-                    ))}
+                <div className="mb-4 flex flex-wrap">
+                    <DndContext>
+                        <SortableContext
+                            items={users.admins.map((x) => x.name)}
+                        >
+                            {users.admins.map((user) => (
+                                <Person
+                                    pfp={user.pfp}
+                                    key={user.name}
+                                    name={user.name}
+                                />
+                            ))}
+                        </SortableContext>
+                    </DndContext>
                 </div>
                 <h2 className="mb-4 text-2xl font-bold  underline">
                     Team Members
                 </h2>
                 <div className="mb-4 flex flex-wrap">
-                    {users.team.map((user) => (
-                        <Chip className="mr-4 mb-4" key={user.name}>
-                            <Pfp
-                                src={user.pfp}
-                                small={true}
-                                className="mr-2 w-6"
-                            />
-                            {user.name}
-                        </Chip>
-                    ))}
+                    <DndContext>
+                        <SortableContext
+                            items={users.team.map((x) => x.name)}
+                            strategy={rectSortingStrategy}
+                        >
+                            {users.team.map((user) => (
+                                <Person
+                                    pfp={user.pfp}
+                                    key={user.name}
+                                    name={user.name}
+                                />
+                            ))}
+                        </SortableContext>
+                    </DndContext>
                 </div>
                 <h2 className="mb-4 text-2xl font-bold  underline">
                     Unsorted:
                 </h2>
                 <div className="mb-4 flex flex-wrap">
-                    {users.unsorted.map((user) => (
-                        <Chip className="mr-4 mb-4" key={user.name}>
-                            <Pfp
-                                src={user.pfp}
-                                small={true}
-                                className="mr-2 w-6"
-                            />
-                            {user.name}
-                        </Chip>
-                    ))}
+                    <DndContext>
+                        <SortableContext
+                            items={users.unsorted.map((x) => x.name)}
+                        >
+                            {users.unsorted.map((user) => (
+                                <Person
+                                    pfp={user.pfp}
+                                    key={user.name}
+                                    name={user.name}
+                                />
+                            ))}
+                        </SortableContext>
+                    </DndContext>
                 </div>
             </main>
         </>
@@ -110,18 +128,50 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+const Person = ({ name, pfp }: { name: string; pfp: string }) => {
+    const {
+        setNodeRef,
+        listeners,
+        attributes,
+        isDragging,
+        transform,
+        transition,
+    } = useSortable({
+        id: name,
+    });
+    const style: CSSProperties = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+        opacity: isDragging ? 0.5 : 1,
+    };
 
-const Chip: FC<PropsWithChildren<{ className?: string }>> = ({
-    children,
-    className,
-}) => {
     return (
-        <div className={clsx("relative z-[9] w-min min-w-max ", className)}>
-            <div className="flex h-full rounded-[15px] border-[3px] border-black bg-blue p-2 px-2 pr-4">
-                {children}
-            </div>
-
-            <div className="absolute top-[2px] left-[2px] z-[-1] h-full w-full rounded-[15px] bg-black"></div>
-        </div>
+        <Chip
+            ref={setNodeRef}
+            style={style}
+            {...listeners}
+            {...attributes}
+            className="mr-4 mb-4"
+        >
+            <Pfp src={pfp} small={true} className="mr-2 h-6" />
+            <div className="whitespace-nowrap">{name}</div>
+        </Chip>
     );
 };
+export const Chip = forwardRef<HTMLDivElement, HTMLProps<HTMLDivElement>>(
+    function Chip({ className, children, ...props }, forwardedRef) {
+        return (
+            <div
+                className={clsx("relative z-[9] w-max  min-w-max", className)}
+                ref={forwardedRef}
+                {...props}
+            >
+                <div className="pr-4i mx-auto flex h-full w-max rounded-[15px] border-[3px] border-black bg-blue p-2 px-2">
+                    {children}
+                </div>
+
+                <div className="absolute top-[2px] left-[2px] z-[-1] h-full w-full rounded-[15px] bg-black"></div>
+            </div>
+        );
+    }
+);
