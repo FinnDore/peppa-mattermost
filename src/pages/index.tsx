50dae2a7edff11fb3942e2c5c90bc2b16dce6e6e
Finn <34718806+FinnDore@users.noreply.github.com>
2023-01-09 21:03:57 +0000

    removed dnd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,7 @@
 /* eslint-disable react/display-name */
-import {
-    DndContext,
-    DragEndEvent,
-    DragOverlay,
-    useDroppable,
-} from "@dnd-kit/core";
-import {
-    rectSortingStrategy,
-    SortableContext,
-    useSortable,
-} from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
 import clsx from "clsx";
 import { type NextPage } from "next";
-import { CSSProperties, forwardRef, HTMLProps, useState } from "react";
+import { forwardRef, HTMLProps } from "react";
 import { Pfp } from "../components/pfp";
 const users = {
     admins: [
@@ -71,147 +59,52 @@ const users = {
 const allUsers = [...users.admins, ...users.team, ...users.unsorted] as const;
 
 const Home: NextPage = () => {
-    const [currentUsers, setUsers] = useState<typeof users>({ ...users });
-    const { setNodeRef: adminsSetNodeRef } = useDroppable({ id: "admin" });
-    const { setNodeRef: teamSetNodeRef } = useDroppable({ id: "team" });
-    const { setNodeRef: unsortedSetNodeRef } = useDroppable({ id: "unsorted" });
-    const [activeId, setActiveId] = useState<string | null>(null);
-    const activeItem = allUsers.find((x) => x.name === activeId);
-    const onDragEnd = (e: DragEndEvent) => {
-        const overId: "admins" | "team" | "unsorted" =
-            e.over?.data.current?.sortable.containerId;
-        const activeId: "admins" | "team" | "unsorted" =
-            e.active.data.current?.sortable.containerId;
-        console.log(e);
-        setActiveId(null);
-
-        if (overId === activeId) return;
-        const currentUser = allUsers.find((x) => x.name === e.active.id);
-        if (!currentUser) return;
-
-        setUsers((prev) => {
-            const newUsers = { ...currentUsers };
-
-            if (!newUsers[overId] || !newUsers[activeId]) return prev;
-
-            newUsers[overId].push(currentUser);
-            newUsers[activeId] = newUsers[activeId].filter(
-                (x) => x.name !== currentUser.name
-            );
-            return newUsers;
-        });
-    };
-
-    const handleDragStart = ({ active }: any) => setActiveId(active.id);
-    const handleDragEnd = ({ active, over }: any) => {
-        setActiveId(null);
-    };
-
     return (
         <>
-            <DndContext
-                onDragEnd={onDragEnd}
-                onDragStart={handleDragStart}
-                onDragCancel={handleDragEnd}
-            >
-                <main>
-                    <h1 className="mb-2 text-4xl font-bold">Users</h1>
-                    <h2 className="mb-4 text-xl font-bold underline">Admins</h2>
-                    <SortableContext
-                        id="admins"
-                        items={currentUsers.admins.map((x) => x.name)}
-                    >
-                        <div
-                            className=" mb-4 flex w-full flex-wrap"
-                            ref={adminsSetNodeRef}
-                        >
-                            {currentUsers.admins.map((user) => (
-                                <Person
-                                    pfp={user.pfp}
-                                    key={user.name}
-                                    name={user.name}
-                                />
-                            ))}
-                        </div>
-                    </SortableContext>
-                    <h2 className="mb-4  text-xl font-bold  underline">
-                        Team Members
-                    </h2>
-                    <SortableContext
-                        id="team"
-                        items={currentUsers.team.map((x) => x.name)}
-                        strategy={rectSortingStrategy}
-                    >
-                        <div
-                            className="mb-4 flex w-full flex-wrap"
-                            ref={teamSetNodeRef}
-                        >
-                            {currentUsers.team.map((user) => (
-                                <Person
-                                    pfp={user.pfp}
-                                    key={user.name}
-                                    name={user.name}
-                                />
-                            ))}
-                        </div>
-                    </SortableContext>
-                    <h2 className="mb-4 text-xl font-bold  underline">
-                        Unsorted:
-                    </h2>
-                    <SortableContext
-                        id="unsorted"
-                        items={currentUsers.unsorted.map((x) => x.name)}
-                    >
-                        <div
-                            className="mb-4 flex w-full flex-wrap "
-                            ref={unsortedSetNodeRef}
-                        >
-                            {currentUsers.unsorted.map((user) => (
-                                <Person
-                                    pfp={user.pfp}
-                                    key={user.name}
-                                    name={user.name}
-                                />
-                            ))}
-                        </div>
-                    </SortableContext>
-                </main>
-                <DragOverlay>
-                    {activeItem && (
-                        <Person pfp={activeItem.pfp} name={activeItem.name} />
-                    )}
-                </DragOverlay>
-            </DndContext>
+            <main>
+                <h1 className="mb-2 text-4xl font-bold">Users</h1>
+                <h2 className="mb-4 text-xl font-bold underline">Admins</h2>
+                <div className=" mb-4 flex w-full flex-wrap">
+                    {users.admins.map((user) => (
+                        <Person
+                            pfp={user.pfp}
+                            key={user.name}
+                            name={user.name}
+                        />
+                    ))}
+                </div>
+                <h2 className="mb-4  text-xl font-bold  underline">
+                    Team Members
+                </h2>
+
+                <div className="mb-4 flex w-full flex-wrap">
+                    {users.team.map((user) => (
+                        <Person
+                            pfp={user.pfp}
+                            key={user.name}
+                            name={user.name}
+                        />
+                    ))}
+                </div>
+                <h2 className="mb-4 text-xl font-bold  underline">Unsorted:</h2>
+                <div className="mb-4 flex w-full flex-wrap ">
+                    {users.unsorted.map((user) => (
+                        <Person
+                            pfp={user.pfp}
+                            key={user.name}
+                            name={user.name}
+                        />
+                    ))}
+                </div>
+            </main>
         </>
     );
 };
 
 export default Home;
 const Person = ({ name, pfp }: { name: string; pfp: string }) => {
-    const {
-        setNodeRef,
-        listeners,
-        attributes,
-        isDragging,
-        transform,
-        transition,
-    } = useSortable({
-        id: name,
-    });
-    const style: CSSProperties = {
-        transform: CSS.Transform.toString(transform),
-        transition,
-        opacity: isDragging ? 0.5 : 1,
-    };
-
     return (
-        <Chip
-            ref={setNodeRef}
-            style={style}
-            {...listeners}
-            {...attributes}
-            className="mr-4 mb-4"
-        >
+        <Chip className="mr-4 mb-4">
             <Pfp src={pfp} small={true} className="mr-2 h-6" />
             <div className="whitespace-nowrap">{name}</div>
         </Chip>
